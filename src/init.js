a003a7c8339d4076835b313e01917e3dcd6b127f
PeresvetS <niks280593@gmail.com>
2017-05-30 23:47:45 +0300

    implemented CRUD tasks

--- src/init.js
+++ src/init.js
@@ -5,10 +5,10 @@ export default async () => {
   const models = getModels(connect);
 
   await models.User.sync({ force: true });
+  await models.TaskStatus.sync({ force: true });
   await models.Task.sync({ force: true });
   await models.Tag.sync({ force: true });
   await models.Comment.sync({ force: true });
-  await models.TaskStatus.sync({ force: true });
   await models.TaskTag.sync({ force: true });
   await models.TaskStatus.bulkCreate([
     { name: 'New', statusAvatar: '/images/new.jpg' },
